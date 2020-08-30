a3afd1208409c56c27d9126ff09b8eea6d452634
Girts-arch <63659706+Girts-arch@users.noreply.github.com>
2020-08-30 20:55:53 +0100

    Add files via upload
    If you want to use this build, you have to install node_modules and open gitignore in this project.
    For some reason I could not upload the full build with them installed already as git would not take the files.:(

---
+++ build/precache-manifest.4d190ce7dc3612e6bfbdca38a71cad5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36e34912d582737dabea47a408ab51f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6482ee7fc87677daa4e",
+    "url": "/static/css/main.15fb3d4c.chunk.css"
+  },
+  {
+    "revision": "4784bd6c80f171ac15e1",
+    "url": "/static/js/2.5c1e677b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5c1e677b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6482ee7fc87677daa4e",
+    "url": "/static/js/main.a700c8f8.chunk.js"
+  },
+  {
+    "revision": "30dab1e171d6eabb5341",
+    "url": "/static/js/runtime-main.8e83e83b.js"
+  }
+]);
